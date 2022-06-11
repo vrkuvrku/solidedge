363e2575dd36f3bd1d48ecdeb573c7d7a338655b
vrkuvrku <martin.sarfy@gmail.com>
2022-06-11 20:04:08 +0200

    jsx.jsx

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
 
-serve((req: Request) => new Response("Hello Deno"));
+serve((req: Request) => new Response("BankIdentity"));
+
